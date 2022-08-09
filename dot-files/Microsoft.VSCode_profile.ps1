35e483e28d369d06b3fc5306f13979954b7bcfc4
Gerry Williams <gerryw1389@gmail.com>
2022-08-09 08:25:25 -0500

    update dot files

---
+++ dot-files/Microsoft.VSCode_profile.ps1
@@ -0,0 +1,13 @@
+<#######<Script>#######>
+<#######<Header>#######>
+# Name: PS Profile Script
+# Copyright: Gerry Williams (https://automationadmin.com)
+# License: MIT License (https://opensource.org/licenses/mit)
+# Script Modified from: n/a
+<#######</Header>#######>
+<#######<Body>#######>
+Set-PSReadlineOption -Bellstyle 'none'
+Set-Location "C:\scripts"
+Clear-Host
+<#######</Body>#######>
+<#######</Script>#######>
